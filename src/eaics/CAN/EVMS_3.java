803702afb59aa30945c2ba0b96d99e430c486b92
TroyASDF <troy.burgess@student.curtin.edu.au>
2018-07-10 20:17:10 +0800

    V2.7.3.1
    Just copied the EVMS v2 code into the EVMS v3 class. Need to change the byte order for it to work with EVMS v3. On my to-do list next.

--- src/eaics/CAN/EVMS_3.java
+++ src/eaics/CAN/EVMS_3.java
@@ -20,9 +20,8 @@ public class EVMS_3 extends EVMS
 	public void setAll(CANMessage message)
 	{
 		super.setCharge(message.getByte(1));		
-		super.setVoltage((message.getByte(3) & 0xf0) << 4 + message.getByte(2));
-		
-		super.setCurrent((message.getByte(4) << 4 | (message.getByte(3) & 0x0f)) - 2048);	//current has 2028 added to it		
+		super.setVoltage( message.getByte(2) + ((message.getByte(3) & 0x0F) << 8) );	
+		super.setCurrent( (((message.getByte(3) & 0xF0) >> 4) + (message.getByte(4) << 4)) - 2048 );	//current has 2028 added to it		
 		super.setAuxVoltage(message.getByte(5) / 10.0);	//tenths of a volt
 		super.setLeakage(message.getByte(6));
 		super.setTemp(message.getByte(7));
