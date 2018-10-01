5535be162a4a7b6cbf300efaf543f7cd4224ea0e
TroyASDF <troy.burgess@student.curtin.edu.au>
2018-10-01 12:21:30 +0800

    V2.8.0.5
    No updates, just fixing tabbing and spaces for report writing.

--- src/eaics/FILE/FileWriterCSV.java
+++ src/eaics/FILE/FileWriterCSV.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package eaics.FILE;
 
 import eaics.CAN.EVMS;
@@ -28,120 +23,120 @@ public class FileWriterCSV implements FileWriter
     
     public FileWriterCSV(String fileName, int type)
     {	
-	try
-	{
-	    fileStrm = new FileOutputStream(fileName);		//Open the file
-	    writer = new OutputStreamWriter(fileStrm, "utf-8");		//Create writer to write stream
-	    bufWriter = new BufferedWriter(writer);		//To write the stream one line at a time
-	    
-	    if(type == 0)
-	    {
-		writeHeadings();
-	    }
-	    else if(type == 1)
-	    {
-		writeRPMHeadings();
-	    }
-	}
-	catch(IOException e)
-	{
-	    e.printStackTrace();
-	}
+		try
+		{
+			fileStrm = new FileOutputStream(fileName);		//Open the file
+			writer = new OutputStreamWriter(fileStrm, "utf-8");		//Create writer to write stream
+			bufWriter = new BufferedWriter(writer);		//To write the stream one line at a time
+			
+			if(type == 0)
+			{
+				writeHeadings();
+			}
+			else if(type == 1)
+			{
+				writeRPMHeadings();
+			}
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
     }
     
     private void writeHeadings()
     {
-	String columnNames = "";
-	
-	columnNames += "Date & Time, ";
-	columnNames += "Current, ";
-	columnNames += EVMS.getLoggingHeadings();
-	columnNames += EVMS_v3.getLoggingHeadings();
-	
-	for(int ii = 1; ii <= 24; ii++)
-	{
-	    for(int jj = 1; jj <= 12; jj++)
-	    {
-		columnNames += "BMS " + ii + " Cell " + jj + " Voltage, ";			    
-	    }
-	    columnNames += "BMS " + ii + " Temp 1, ";
-	    columnNames += "BMS " + ii + " Temp 2, ";
-	}
-	for(int ii = 1; ii <= 4; ii++)
-	{
-	    columnNames += "ESC " + ii + " Battery Voltage, ";
-	    columnNames += "ESC " + ii + " Battery Current, ";
-	    columnNames += "ESC " + ii + " RPM, ";
-	    columnNames += "ESC " + ii + " Odometer, ";
-	    columnNames += "ESC " + ii + " Controller Temperature, ";
-	    columnNames += "ESC " + ii + " Motor Temperature, ";
-	    columnNames += "ESC " + ii + " Battery Temperature, ";
-	    columnNames += "ESC " + ii + " Requested Output PWM, ";
-	    columnNames += "ESC " + ii + " Real Output PWM, ";
-	    columnNames += "ESC " + ii + " Warnings, ";
-	    columnNames += "ESC " + ii + " Failures, ";
-	    columnNames += "ESC " + ii + " Remaining Battery Capacity, ";
-	    columnNames += "ESC " + ii + " Throttle Command, ";			
-	}
-	columnNames += "Load Cell\n";
+		String columnNames = "";
+		
+		columnNames += "Date & Time, ";
+		columnNames += "Current, ";
+		columnNames += EVMS.getLoggingHeadings();
+		columnNames += EVMS_v3.getLoggingHeadings();
+		
+		for(int ii = 1; ii <= 24; ii++)
+		{
+			for(int jj = 1; jj <= 12; jj++)
+			{
+				columnNames += "BMS " + ii + " Cell " + jj + " Voltage, ";			    
+			}
+			columnNames += "BMS " + ii + " Temp 1, ";
+			columnNames += "BMS " + ii + " Temp 2, ";
+		}
+		for(int ii = 1; ii <= 4; ii++)
+		{
+			columnNames += "ESC " + ii + " Battery Voltage, ";
+			columnNames += "ESC " + ii + " Battery Current, ";
+			columnNames += "ESC " + ii + " RPM, ";
+			columnNames += "ESC " + ii + " Odometer, ";
+			columnNames += "ESC " + ii + " Controller Temperature, ";
+			columnNames += "ESC " + ii + " Motor Temperature, ";
+			columnNames += "ESC " + ii + " Battery Temperature, ";
+			columnNames += "ESC " + ii + " Requested Output PWM, ";
+			columnNames += "ESC " + ii + " Real Output PWM, ";
+			columnNames += "ESC " + ii + " Warnings, ";
+			columnNames += "ESC " + ii + " Failures, ";
+			columnNames += "ESC " + ii + " Remaining Battery Capacity, ";
+			columnNames += "ESC " + ii + " Throttle Command, ";			
+		}
+		columnNames += "Load Cell\n";
 
-	write(columnNames);
+		write(columnNames);
     }
     
     private void writeRPMHeadings()
     {
-	String columnNames = "";
-	
-	columnNames += "Date & Time, ";
-	columnNames += "Current, ";
-	columnNames += EVMS.getLoggingHeadings();
-	columnNames += EVMS_v3.getLoggingHeadings();
-	
-	for(int ii = 1; ii <= 1; ii++)
-	{
-	    columnNames += "ESC " + ii + " Battery Voltage, ";
-	    columnNames += "ESC " + ii + " Battery Current, ";
-	    columnNames += "ESC " + ii + " RPM, ";
-	    columnNames += "ESC " + ii + " Odometer, ";
-	    columnNames += "ESC " + ii + " Controller Temperature, ";
-	    columnNames += "ESC " + ii + " Motor Temperature, ";
-	    columnNames += "ESC " + ii + " Battery Temperature, ";
-	    columnNames += "ESC " + ii + " Requested Output PWM, ";
-	    columnNames += "ESC " + ii + " Real Output PWM, ";
-	    columnNames += "ESC " + ii + " Warnings, ";
-	    columnNames += "ESC " + ii + " Failures, ";
-	    columnNames += "ESC " + ii + " Remaining Battery Capacity, ";
-	    columnNames += "ESC " + ii + " Throttle Command, ";			
-	}
-	columnNames += "Power (kw)\n";
+		String columnNames = "";
+		
+		columnNames += "Date & Time, ";
+		columnNames += "Current, ";
+		columnNames += EVMS.getLoggingHeadings();
+		columnNames += EVMS_v3.getLoggingHeadings();
+		
+		for(int ii = 1; ii <= 1; ii++)
+		{
+			columnNames += "ESC " + ii + " Battery Voltage, ";
+			columnNames += "ESC " + ii + " Battery Current, ";
+			columnNames += "ESC " + ii + " RPM, ";
+			columnNames += "ESC " + ii + " Odometer, ";
+			columnNames += "ESC " + ii + " Controller Temperature, ";
+			columnNames += "ESC " + ii + " Motor Temperature, ";
+			columnNames += "ESC " + ii + " Battery Temperature, ";
+			columnNames += "ESC " + ii + " Requested Output PWM, ";
+			columnNames += "ESC " + ii + " Real Output PWM, ";
+			columnNames += "ESC " + ii + " Warnings, ";
+			columnNames += "ESC " + ii + " Failures, ";
+			columnNames += "ESC " + ii + " Remaining Battery Capacity, ";
+			columnNames += "ESC " + ii + " Throttle Command, ";			
+		}
+		columnNames += "Power (kw)\n";
 
-	write(columnNames);
+		write(columnNames);
     }
     
     @Override
     public void write(String data) 
     {
-	try
-	{
-	    bufWriter.write(data);
-	    bufWriter.flush();	//flush the writer
+		try
+		{
+			bufWriter.write(data);
+			bufWriter.flush();	//flush the writer
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
 	}
-	catch(IOException e)
+		
+	@Override
+	public void close()
 	{
-	    e.printStackTrace();
-	}
-    }
-    
-    @Override
-    public void close()
-    {
-	try
-	{
-	    bufWriter.close();	    
-	}
-	catch(Exception e)
-	{
-	    
-	}	
+		try
+		{
+			bufWriter.close();	    
+		}
+		catch(Exception e)
+		{
+			
+		}	
     }
 }
